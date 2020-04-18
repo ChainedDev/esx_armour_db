f7d396e3810117f4362b54a663d525ad9ec7fd0b
ChainedDev <44199939+ChainedDev@users.noreply.github.com>
2020-04-18 04:45:22 -0400

    small cleanup
    variable rename, minor optimization, and false ban prevention

--- client/client.lua
+++ client/client.lua
@@ -1,20 +1,28 @@
-local citizen = false
+local playerLoaded = false
 
 RegisterNetEvent('LRP-Armour:Client:SetPlayerArmour')
 AddEventHandler('LRP-Armour:Client:SetPlayerArmour', function(armour)
     Citizen.Wait(10000)  -- Give ESX time to load their stuff. Because some how ESX remove the armour when load the ped.
     SetPedArmour(PlayerPedId(), tonumber(armour))
-    citizen = true
+    playerLoaded = true
 end)
 
 local TimeFreshCurrentArmour = 60000  -- 60 seconds
 
+local armor = 0
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
-        if citizen == true then
-            TriggerServerEvent('LRP-Armour:Server:RefreshCurrentArmour', GetPedArmour(PlayerPedId()))
+            
+        if playerLoaded then
+            armor = GetPedArmour(PlayerPedId()
+            if armor <= 200 then
+                TriggerServerEvent('LRP-Armour:Server:RefreshCurrentArmour', armor)
+            end
             Citizen.Wait(TimeFreshCurrentArmour)
+        else
+            Citizen.Wait(500)
         end
+                
     end
 end)
